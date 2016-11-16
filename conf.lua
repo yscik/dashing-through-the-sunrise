2bd130f8a86bb091a16694aaf13f12a5decb40c6
Peter Kiss <peter@yscik.com>
2016-11-16 02:29:59 +0100

    build config

--- conf.lua
+++ conf.lua
@@ -1,6 +1,19 @@
 function love.conf(t)
-    t.window.title = "Höme"
-    t.window.width = 1900
-    t.window.height = 1000
-    t.window.msaa = 8
+  t.window.title = "Dashing through the sunrise"
+  t.window.fullscreen = true
+  t.window.fullscreentype = "desktop"
+  t.window.msaa = 8
+  
+  t.releases = {
+    title = "Dashing through the sunrise",              -- The project title (string)
+    package = 'DashingThroughSunrise',            -- The project command and package name (string)
+    loveVersion = '0.10.2',        -- The project LÖVE version
+    version = '0.2',            -- The project version
+    author = 'Peter Kiss',             -- Your name (string)
+    email = 'peter@yscik.com',              -- Your email (string)
+    description = '',        -- The project description (string)
+    homepage = 'http://badpoet.itch.io/dashing-through-the-sunrise',           -- The project homepage (string)
+    identifier = 'io.badpoet.sunrise',         -- The project Uniform Type Identifier (string)
+    releaseDirectory = 'build',   -- Where to store the project releases (string)
+  }
 end
